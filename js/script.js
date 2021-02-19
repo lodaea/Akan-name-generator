97089da9a37d62b6d816e07643fb83bb304aecbd
K.Anais <kiamanais@gmail.com>
2021-02-19 20:16:42 +0100

    getName function

--- js/script.js
+++ js/script.js
@@ -1,10 +1,77 @@
 let outputTag = document.getElementById('output')
 
-function getName(event){
+
+function getName(event) {
     event.preventDefault()
     let date = document.getElementById('date').value
     let dateOfBirth = new Date(date).toDateString()
-    let message = `${dateOfBirth}`
+    let day = dateOfBirth.split(' ')[0]
+    let gender = document.getElementById('gender').value
+    
+    let akanName;
+
+    let maleAkanNames = [
+        "Kwasi",
+        "kwadwo",
+        "Kwabena",
+        "Kwaku",
+        "Yaw",
+        "Kofi",
+        "Kwame"
+    ];
+    let femaleAkanNames = [
+        "Akosua",
+        "Adwoa",
+        "Abenaa",
+        "Akua",
+        "Yaa",
+        "Afua",
+        "Ama" 
+    ];
+    
+    
+    if ( day === "Sun" && gender === "female") {
+        akanName = femaleAkanNames[0];
+    } else if (day === "Sun" && gender === "male") {
+        akanName = maleAkanNames[0];
+
+
+    } else if (day ==="Mon" && gender === "female") {
+        akanName = femaleAkanNames[1];
+    } else if (day === "Mon" && gender === "male") {
+        akanName = maleAkanNames[1];
+
+
+    } else if (day === "Tue" && gender === "female") {
+        akanName = femaleAkanNames[2];
+    } else if (day === "Tue" && gender === "male") {
+        akanName = maleAkanNames[2];
+
+
+    } else if (day === "Wed" && gender === "female") {
+        akanName = femaleAkanNames[3];
+    } else if (day === "Wed" && gender === "male") {   
+        akanName = maleAkanNames[3];
+
+
+    } else if (day === "Thu" && gender === "female") {
+        akanName = femaleAkanNames[4];
+    } else if (day === "Thu" && gender === "male") {
+        akanName = maleAkanNames[4];
+
+
+    } else if (day === "Fri" && gender === "female") {
+        akanName = femaleAkanNames[5];
+    } else if (day === "Fri" && gender === "male") {  
+        akanName = maleAkanNames[5];
+   
+   
+    } else if (day === "Sat" && gender === "female") {
+            akanName = femaleAkanNames[6];
+    } else if (day === "Sat" && gender === "male") {
+            akanName = maleAkanNames[6]
+    }
+
+    let message = `you were born on a ${day}; your name is ${akanName}`
     outputTag.innerHTML = message
 }
-
