e9f78b13fb3f496a70eb30deb65abc645c2d4397
K.Anais <kiamanais@gmail.com>
2021-02-19 15:51:08 +0100

    added name arrays

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const maleAkanNames = [
+    "Kwame",
+    "Kwasi",
+    "Kwadwo",
+    "Kwabena",
+    "Kwaku",
+    "Yaw",
+    "Kofi"
+];
+const femaleAkanNames = [
+    "Ama",
+    "Akosua",
+    "Adwoa",
+    "Abenaa",
+    "Akua",
+    "Yaa",
+    "Afua"
+];
