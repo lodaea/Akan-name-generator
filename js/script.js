93945a839d7567649cdef5c049b22b1ac1b04611
K.Anais <kiamanais@gmail.com>
2021-02-19 18:55:05 +0100

    removed arrays

--- js/script.js
+++ js/script.js
@@ -1,28 +1,9 @@
-const maleAkanNames = [
-    "Kwasi",
-    "kwadwo",
-    "Kwabena",
-    "Kwaku",
-    "Yaw",
-    "Kofi",
-    "Kwame",
-];
-const femaleAkanNames = [
-    "Akosua",
-    "Adwoa",
-    "Abenaa",
-    "Akua",
-    "Yaa",
-    "Afua"
-    "Ama",
-];
-
 let outputTag = document.getElementById('output')
 
 function getName(event){
     event.preventDefault()
     let date = document.getElementById('date').value
-    let dateOfBirth = new Date(date)
+    let dateOfBirth = new Date(date).toDateString()
     let message = `${dateOfBirth}`
     outputTag.innerHTML = message
 }
